5b94b0736c160b92b8cb9b9b14b9bfbf54e6b94f
이요한 <icb1696@naver.com>
2025-11-04 10:48:16 +0900

    Fix: import 오류 수정

--- src/domain/post/dto/list-posts-query.dto.ts
+++ src/domain/post/dto/list-posts-query.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, Min } from 'class-validator';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class PostsPageQueryDto {
   @IsOptional()
