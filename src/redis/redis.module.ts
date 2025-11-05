448a6cb744f3803af9e9b91b9cd206ef524975ae
이요한 <icb1696@naver.com>
2025-11-06 00:43:23 +0900

    Fix: redisioadapter provider 누락분 추가

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -3,7 +3,7 @@ import { RedisService } from './redis.service';
 import { RedisIoAdapter } from './redis-io.adapter';
 
 @Module({
-  providers: [RedisService], //  RedisIoAdapter 제거??
+  providers: [RedisService, RedisIoAdapter], //  RedisIoAdapter 제거??
   exports: [RedisService, RedisIoAdapter],
 })
 export class RedisModule {}
