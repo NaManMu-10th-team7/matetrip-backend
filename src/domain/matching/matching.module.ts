6caa329f5559d0caa3607ad9449813abd2a6325e
YooSungSoo <elcane2@naver.com>
2025-11-09 00:20:18 +0900

    feat:매칭 알고리즘 가중치 구현

--- src/domain/matching/matching.module.ts
+++ src/domain/matching/matching.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchingController } from './matching.controller';
 import { MatchingService } from './matching.service';
 import { MatchingProfile } from './entities/matching-profile.entity';
+import { Profile } from '../profile/entities/profile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MatchingProfile])],
+  imports: [TypeOrmModule.forFeature([MatchingProfile, Profile])],
   controllers: [MatchingController],
   providers: [MatchingService],
   exports: [MatchingService],
