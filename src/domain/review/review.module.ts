a4a71c76f8e5b6d80c7983c9b0bb3cc2613d1023
YooSungSoo <elcane2@naver.com>
2025-11-13 14:15:59 +0900

    feat: 매너온도 계산 로직 추가

--- src/domain/review/review.module.ts
+++ src/domain/review/review.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Review } from './entities/review.entity';
-import { Post } from '../post/entities/post.entity';
-import { Users } from '../users/entities/users.entity';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { Workspace } from '../workspace/entities/workspace.entity';
+import { Profile } from '../profile/entities/profile.entity';
 
 @Module({
   imports: [
     // TypeORM 리포지토리를 DI로 사용하기 위해 forFeature 등록
-    TypeOrmModule.forFeature([Review, Post, Users, Workspace]),
+    TypeOrmModule.forFeature([Review, Workspace, Profile]),
   ],
   controllers: [ReviewController],
   providers: [ReviewService], // 서비스 프로바이더 등록
