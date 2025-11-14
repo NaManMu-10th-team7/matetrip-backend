b7e1f35c2f6674b787c9c5077e9fb403e7cb4bc3
SIHYEONPARK <127606365+EPSIHYEON@users.noreply.github.com>
2025-11-14 21:30:34 +0900

    Feat: 사진 올리기 구현  (#62)
    * Feat: 임베딩 과정 바꿈 + 게시글반환안하고 userId랑 score만 반환
    
    * Feat: 주석추가
    
    * Fix: 숫자 조정 및 변수 이름 변경
    
    변수가 프론트명이랑 안맞았음
    
    * Fix: 카테고리 약간 수정
    
    * Feat:게시글 사진 [1]
    
    * Feat: 게시글 사진 추가 백엔드
    
    * Fix: 임베딩 관련
    
    * Feat: 리뷰어들에게 profileImageId 전달
    
    * Fix: 코드 래빗 반영
    
    * Feat: API 한번에 통합

--- src/domain/matching/dto/embedding-matching-profile.dto.ts
+++ src/domain/matching/dto/embedding-matching-profile.dto.ts
@@ -1,14 +1,14 @@
 import {
-  // ArrayNotEmpty,
-  // ArrayUnique,
-  // IsArray,
-  // IsEnum,
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsArray,
+  IsEnum,
   IsOptional,
   IsString,
   IsUUID,
 } from 'class-validator';
-// import { TravelStyleType } from '../../profile/entities/travel-style-type.enum';
-// import { TendencyType } from '../../profile/entities/tendency-type.enum';
+import { TravelStyleType } from '../../profile/entities/travel-style-type.enum';
+import { TendencyType } from '../../profile/entities/tendency-type.enum';
 
 export class EmbeddingMatchingProfileDto {
   @IsOptional()
@@ -19,17 +19,17 @@ export class EmbeddingMatchingProfileDto {
   @IsString()
   description?: string;
 
-  // @IsOptional()
-  // @IsArray()
-  // @ArrayUnique()
-  // @ArrayNotEmpty()
-  // @IsEnum(TravelStyleType, { each: true })
-  // travelTendencyTypes?: TravelStyleType[];
+  @IsOptional()
+  @IsArray()
+  @ArrayUnique() // 같은 값이 있으면 에러 반환
+  @ArrayNotEmpty()
+  @IsEnum(TravelStyleType, { each: true })
+  travelTendencyTypes?: TravelStyleType[];
 
-  // @IsOptional()
-  // @IsArray()
-  // @ArrayUnique()
-  // @ArrayNotEmpty()
-  // @IsEnum(TendencyType, { each: true })
-  // travelTendencies?: TendencyType[];
+  @IsOptional()
+  @IsArray()
+  @ArrayUnique()
+  @ArrayNotEmpty()
+  @IsEnum(TendencyType, { each: true })
+  travelTendencies?: TendencyType[];
 }
