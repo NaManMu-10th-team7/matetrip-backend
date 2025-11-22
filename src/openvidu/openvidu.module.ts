a70a5ab1e7593e457247e8529f7ec2ddbde087cc
SungSoo Yoo <90916588+YooSungSoo@users.noreply.github.com>
2025-11-22 18:06:53 +0900

    Feat/facechat (#107)
    * feat:AWS chime이용 화상 채팅 구현
    
    * fix:openvidu 관련 파일 및 로직 제거

--- src/openvidu/openvidu.module.ts
+++
@@ -1,9 +0,0 @@
-// import { Module } from '@nestjs/common';
-// import { OpenViduService } from './openvidu.service';
-// import { OpenViduController } from './openvidu.controller';
-
-// @Module({
-//   controllers: [OpenViduController],
-//   providers: [OpenViduService],
-// })
-// export class OpenviduModule {}
